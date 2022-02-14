d8b1cd8f4b41132b9b8c8d60beef6cbeaa4ecc1c
Chen <Chenpeleg>
2022-02-15 00:00:18 +0200

    added button

--- src/components/Help.svelte
+++ src/components/Help.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+const data: string = '.<br> This is an aditional Data';
+const click_button = (event: Event) => {
+  alert('clicked');
+};
 </script>
 
-<main class="help-component">this is my help component</main>
+<main class="help-component">
+  this is my help component {@html data}
+
+  <br />
+  <button on:click="{click_button}">Click me!</button>
+</main>
 
 <style>
 main.help-component {
@@ -10,7 +19,7 @@ main.help-component {
   padding: 1em;
   margin: 1em;
   width: 200px;
-  height: 150px;
+  min-height: 150px;
   background-color: rgb(16, 8, 32);
 }
 </style>
